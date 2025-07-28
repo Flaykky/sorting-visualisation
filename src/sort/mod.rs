d1d8edfbb1a121c029e784e900d3a821eac599dc
flaykDev <138144836+Flaykky@users.noreply.github.com>
2025-07-28 16:34:53 +0300

    Update mod.rs

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -3,10 +3,13 @@ pub mod mergesort;
 pub mod timsort;
 pub mod radix;
 pub mod heapsort;
-
-// pub mod bubblesort;
-// pub mod insertionsort;
-// pub mod cocktailsort;
+pub mod bubblesort;
+pub mod selectionsort;
+pub mod insertionsort;
+pub mod gnomesort;
+pub mod countingsort;
+pub mod cocktailsort;
+pub mod shellsort; 
 
 pub use self::quicksort::quicksort;
 pub use self::mergesort::mergesort;
